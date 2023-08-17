be385cb37943804f592e1221cff1fde1c74c21b8
Rodrigou0 <127239242+Rodrigou0@users.noreply.github.com>
2023-08-17 09:13:49 +0000

    jwt exercise

--- src/app.py
+++ src/app.py
@@ -11,6 +11,8 @@ from api.models import db
 from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from flask_jwt_extended import JWTManager
+
 
 #from models import Person
 
@@ -30,8 +32,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db, compare_type = True)
 db.init_app(app)
 
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET")
+jwt = JWTManager(app)
+
 # Allow CORS requests to this API
-CORS(app)
+CORS(app, origins='https://rodrigou0-redesigned-lamp-44xxpxgrgq6fj9g-3000.preview.app.github.dev')
 
 # add the admin
 setup_admin(app)
---
+++ src/front/js/pages/login.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { Context } from "../store/appContext";
+
+const Login = () => {
+	const { store, actions } = useContext(Context);
+
+	const [email, setEmail] = useState();
+	const [password, setPassword] = useState();
+
+	const navigate = useNavigate();
+
+	const onSubmit =(e)=>{
+		e.preventDefault()
+		if(email === ''){
+			alert('email not found')
+		}else if(password === ''){
+			alert('password is empty')
+		}else{
+			console.log("inside fetch");
+			fetch(`http://127.0.0.1:3001/api/login`,
+			{ 
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ email, password }) 
+     		}).then((res)=> res.json())
+			.then((result)=>{
+				console.log('token is here =====>', result);
+				localStorage.setItem("jwt-token", result.token);
+				alert('You are logged in!')
+				navigate('/')
+			}).catch((err)=> {
+				console.log(err);
+			})
+		}
+	}
+
+	return (
+		<div className="container">
+			<form>
+				<div class="mb-3">
+					<label for="exampleInputEmail1" class="form-label">Email address</label>
+					<input 
+						type="email"
+						class="form-control"
+						id="exampleInputEmail1"
+						aria-describedby="emailHelp"
+						value={email}
+						onChange={(e)=> setEmail(e.target.value)}
+					/>
+					<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+				</div>
+				<div class="mb-3">
+					<label for="exampleInputPassword1" class="form-label">Password</label>
+					<input 
+						type="password" 
+						class="form-control" 
+						id="exampleInputPassword1"
+						value={password}
+						onChange={(e)=> setPassword(e.target.value)}
+					/>
+				</div>
+				<div>
+					<button type="submit" class="btn btn-primary" onClick={onSubmit}>Submit</button>
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default Login
